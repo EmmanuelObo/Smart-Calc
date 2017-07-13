7fa75545e3b4a483565d559eeb15f07de1ada9f2
Emmanuel Obogbaimhe <eobogbai@buffalo.edu>
2017-07-12 23:40:13 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,4 +22,54 @@ export class AppComponent {
 
   hello:string = this.talk();
 
+  addFive(value){
+
+    if(value=='Emmanuel')
+       return 'Hey Thats Me!'
+
+     value = +value
+
+     return value+5;
+  }
+
+  addition(value)
+  {
+    var values = value.split(" ");
+    var total = 0
+
+    for(var index = 0 ; index < values.length ; index++)
+      {  
+        if(values[index]=='+')
+        {
+          total += +values[index-1];
+        }
+        else if(values[index]=='-')
+        {
+          total -= +values[index-1];
+        }
+        else if(values[index]=='/')
+        {
+          total /= +values[index-1];
+        }
+        else if(values[index]=='*')
+        {
+          total *= +values[index-1];
+        }
+         
+      }
+    return total;
+  }
+
+  operator(sign){
+    if(sign=='+' || 
+      sign=='-' ||
+      sign=='/' ||
+      sign=='-')
+    {
+      return true;
+    }
+    else
+      return false;
+  }
+
 }
