009aac74325760094de0453b9ae9b88ef829bbd8
Emmanuel Obogbaimhe <eobogbai@buffalo.edu>
2017-07-13 00:40:38 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,32 +32,47 @@ export class AppComponent {
      return value+5;
   }
 
-  addition(value)
+  calculation(value)
   {
     var values = value.split(" ");
-    var total = 0
+    var total = +values[0]
 
-    for(var index = 0 ; index < values.length ; index++)
+    for(var index = 1 ; index < values.length ; index++)
       {  
         if(values[index]=='+')
         {
-          total += +values[index-1];
+          total += +values[index+1];
         }
         else if(values[index]=='-')
         {
-          total -= +values[index-1];
+          total -= +values[index+1];
         }
         else if(values[index]=='/')
         {
-          total /= +values[index-1];
+          total /= +values[index+1];
         }
         else if(values[index]=='*')
         {
-          total *= +values[index-1];
+          total *= +values[index+1];
         }
          
       }
     return total;
+  }
+
+  readable(input)
+  {
+    var inputList = input.split(" ");
+    var commentary = '( ';
+    commentary += inputList[1] + ' ' + inputList[0];
+
+    for(var index = 0 ; index < inputList.length ; index++)
+    {
+      commentary += inputList[index] + ' ' + inputList[index-1];
+    }
+    commentary += ' )'
+
+    return commentary;
   }
 
   operator(sign){
